8e6e9d5c305f5b6e10dfb034d4aac22485becbd9
Leonardo Thibes <leonardothibes@gmail.com>
2017-03-29 13:36:29 -0300

    Implementando FileversionInterface

---
+++ src/FileversionInterface.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Lidercap\Component\Fileversion;
+
+/**
+ * Interface para controladores de versão de arquivo.
+ */
+interface FileversionInterface
+{
+    /**
+     * @param string $path Caminho para o arquivo.
+     */
+    public function __construct($path = null);
+
+    /**
+     * @return string
+     */
+    public function getPath();
+
+    /**
+     * @param string $path Caminho para o arquivo.
+     *
+     * @return string
+     */
+    public function setPath($path);
+
+    /**
+     * Verifica se a versão atual do arquivo tem
+     * diferenças em relação a sua versão anterior.
+     *
+     * @return bool
+     */
+    public function isUpdated();
+
+    /**
+     * Verifica se a versão atual do arquivo está
+     * desatualizada em relação a sua versão anterior.
+     *
+     * @return bool
+     */
+    public function isOutdated();
+
+    /**
+     * Obtém o número da versão atual do arquivo.
+     *
+     * @return int
+     */
+    public function version();
+
+    /**
+     * Lista todas as versões do arquivo.
+     *
+     * @return array
+     */
+    public function fetch();
+
+    /**
+     * Obtém o conteúdo do arquivo.
+     */
+    public function getContents();
+
+    /**
+     * Escreve no arquivo, gerando uma nova versão.
+     *
+     * @param string $contents
+     *
+     * @return $this
+     */
+    public function putContents($contents);
+
+    /**
+     * Apaga do disco todas as versões do arquivo,
+     * Mantendo somente o número de versões especificado.
+     *
+     * @param int $keep
+     *
+     * @return $this
+     */
+    public function clear($keep = 3);
+}
