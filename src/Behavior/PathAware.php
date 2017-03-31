bb3565911d421529c013c2acff0a08a239933e8b
Leonardo Thibes <leonardothibes@gmail.com>
2017-03-31 11:36:45 -0300

    update

--- src/Behavior/PathAware.php
+++ src/Behavior/PathAware.php
@@ -2,15 +2,24 @@
 
 namespace Lidercap\Component\Fileversion\Behavior;
 
-/**
- * @codeCoverageIgnore
- */
 trait PathAware
 {
     /**
      * @var string
      */
     protected $path;
+
+    /**
+     * @param string $path Caminho para o arquivo.
+     *
+     * @return $this
+     */
+    public function setPath($path)
+    {
+        $this->path = $this->suffixRemove($path);
+
+        return $this;
+    }
 
     /**
      * @return string
@@ -21,10 +30,21 @@ trait PathAware
     }
 
     /**
-     * @param string $path Caminho para o arquivo.
-     */
-    public function setPath($path)
+     * Remove a versão do caminho ou nome do arquivo quando necessário.
+     *
+     * @param string $path
+     *
+     * @return string
+     **/
+    protected function suffixRemove($path)
     {
-        $this->path = $path;
+        $exploded = (array)@explode('.', $path);
+        $version  = end($exploded);
+
+        if (is_numeric($version)) {
+            $path = str_replace('.' . $version, '', $path);
+        }
+
+        return $path;
     }
 }
